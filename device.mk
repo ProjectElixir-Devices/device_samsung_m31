7c4514302b96dc144d89c7f749530c0f4199f32a
roynatech2544 <whiteshell2544@naver.com>
2023-01-19 20:04:56 +0900

    m31: Setup overlays

--- device.mk
+++ device.mk
@@ -14,10 +14,14 @@
 # limitations under the License.
 #
 
+DEVICE_PATH := device/samsung/m31
+
 # Inherit common device configuration
 $(call inherit-product, device/samsung/universal9611-common/common.mk)
 # Inherit A51 blobs
 $(call inherit-product, vendor/samsung/m31/m31-vendor.mk)
+
+DEVICE_PACKAGE_OVERLAYS += $(DEVICE_PATH)/overlay
 
 # Screen H/W
 TARGET_SCREEN_HEIGHT := 2400
