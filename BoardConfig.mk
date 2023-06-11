48b5cf5336ca1839c1d769a1396b46a4a02c85b7
roynatech2544 <whiteshell2544@naver.com>
2023-06-11 16:32:16 +0900

    m31: Remove TARGET_KERNEL_CONFIG from here

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -24,8 +24,6 @@ include device/samsung/universal9611-common/BoardConfigCommon.mk
 # OTA Asserts
 TARGET_OTA_ASSERT_DEVICE := m31,m31dd,m31nsxx
 
-# Kernel config
-TARGET_KERNEL_CONFIG := vendor/m31-aosp_defconfig
 
 ## Partitions Sizes
 BOARD_BOOTIMAGE_PARTITION_SIZE := 61865984
